d2d79cfff41644ae0c69a0448488e684733b749b
MuskaanDev <muskaanwork24@gmail.com>
2024-06-28 04:00:37 +0530

    Added comments

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,18 @@
+/* #Primitve:7 categories mean call by value which mean changes made in copy and not in the real data
+String,Number,Boolean,Null,undefiend,symbol,bigint
+
+don't have to define datatype while declaring the variable
+
+#Reference type - non primitive
+array,objects,functions
+
+object declaration 
+let myobj ={
+name:"muskaan",
+age:22,}
+
+func declaration
+const myfunc = function(){
+somsole.log("heloo world");}
+
+//dstatype of non primitve functions is object more like object functions*/
