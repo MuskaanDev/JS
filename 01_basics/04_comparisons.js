cce2cdc79c207992734f523c3ba135823ed6653e
MuskaanDev <muskaanwork24@gmail.com>
2024-06-28 01:09:53 +0530

    Initial commit

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,9 @@
+console.log(2>1)//boolean output
+
+console.log("2">1)//output true, js automatically converts 2 into a number
+console.log(null>0)//for > false, == false,>= true because comparison opeartors convert null to a number that is zero but equality operator does not
+
+
+//for undefined all operations always give false
+
+// strict check === checks not only values but datatypes also
