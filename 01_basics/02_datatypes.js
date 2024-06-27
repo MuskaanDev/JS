2d7d6be2ccddb343d59c37c459b7f38955e1c90e
MuskaanDev <muskaanwork24@gmail.com>
2024-06-27 20:49:45 +0530

    Initial commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+//using nide js not browser
+
+console.log(3+3)
+console.log("muskaan")
+
+let name="muskaan"
+let age="21"
+let isloggedin= false
+
+//number ->2 to the power 53
+//string ->""
+//boolean->true or false
+//null is a standalone value->value is empty
+//undefined->when value is not assigned
+//symbol mostly used in react->used to fined uniqueness
+//object
+
+console.log(typeof "muskaan");
