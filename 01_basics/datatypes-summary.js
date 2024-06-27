06cbc1ea0209a8db7129a02cc011853fc0a905b3
MuskaanDev <muskaanwork24@gmail.com>
2024-06-28 04:11:33 +0530

    stack and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -16,3 +16,26 @@ const myfunc = function(){
 somsole.log("heloo world");}
 
 //dstatype of non primitve functions is object more like object functions*/
+
+//******************************************************* */
+
+/* Stack(Primitive,copied value),Heap(non-primitve,refernce means original value)
+let my youtubename ="muskaanhello" this goes into stack
+let anothername = myyoutubename
+
+console.log(myyotubename)//output will be muskaan hello
+but 
+anothername="muskaanworld"
+console.log(anothername)//output muskaanworld because changes made on copy and not on actual value thats why even if we gave refernce to myyotubename its value doesnt change because a copy changes not the original value
+
+let userone={
+email=nkqsbcj   ///object
+upi=sduyvduv}
+
+let usertwo=userone
+usertwo.email=vuhvvw //email changed
+
+console.log(userone.email)
+console.log(usertwo.email) //same output because changes made in actual copy
+
+*/
