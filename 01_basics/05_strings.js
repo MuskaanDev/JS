b21653b424a97900404d6a14048628137d419957
MuskaanDev <muskaanwork24@gmail.com>
2024-07-06 03:46:43 +0530

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,30 @@
+//string can be dentoted using single or double quotes
+ const name="hitesh"
+ const repocount = 6
+  console.log(name+repocount+"value"); // concatenation
+
+  //modern syntax
+  console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+  //string declare method 2 
+  const name =  new String('muskaan')
+
+  //string type is an object
+
+
+  console.log(gameName[0]);
+  //can access methods and function using name.length
+  //name.toUpperCase etc.
+  //to find index .charAt
+  //to find which position .indeXOf
+
+  //to get substring .substring(0,4) 4 not included ans will be musk
+  //same can be done with slice but in slice we can give negative value starts with back
+
+  //to trim extra spaces mostly used when user enters input .trim
+  //browser doesnt understand spaces convert them into urlm format
+
+  //url.replace('%20', '-')
+  //url.includes('muskaan) exists or not? bool value
+
+  //name.split('-')  to convert into array and split on the basis of dash can split on the basis of anything
