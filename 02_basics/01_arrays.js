9148a4085b894637769066521216a0a1d9b763f0
MuskaanDev <muskaanwork24@gmail.com>
2024-07-07 04:12:51 +0530

    arrays first

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+const myarr=[0,1,2,3,4]
+
+//array is resizeable
+//mix datatypes
+console.log(myarr[0])
+
+//copy operations create a shallow copy that have a same reference point
+
+const heroes=["sdw","sdfw"]
+const arr=new Array(1,2,3,4)
+
+//you get length and protoytpes in arrays
+
+//emthods
+
+myarr.push(6)
+myarr.pop()//last value removed
+
+myarr.unshift(0) //adds in the end but time consuming
+myarr.shift(0)//removes first value
+
+console.log(myarr.includes(0))
+console.log(myarr.indexOf(3))
+
+const newarr=myarr.join()//binds the array and converts it into a string
+console.log("A ",myarr)
