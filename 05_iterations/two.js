efac3c32de9a417d318bbb0c053d0097ced190bf
MuskaanDev <muskaanwork24@gmail.com>
2024-08-16 11:51:45 +0530

    iterations updated

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+
+let index = 0
+while(index<=10){
+console.log("hello");
+
+index = index+2;
+}
+
+let arra = ["hello","hi","bye"]
+
+let arr = 0
+while(arr,arra.length){
+    console.log(`Value is ${arra[arr]}`);
+    arr=arr+1
+}
+
+let score =1
+do{
+console.log(`score is ${score}`);
+score++;
+}while(score<=10);//work first check condition later
